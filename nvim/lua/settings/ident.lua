733f13595d3aa2e02454dfbac44801fd3025c057
Livins1 <latemantis@gmail.com>
2022-01-17 22:00:36 +0800

    Update ident_blankline config

--- nvim/lua/settings/ident.lua
+++ nvim/lua/settings/ident.lua
@@ -1,10 +1,13 @@
 vim.opt.list = true
 vim.opt.listchars:append("eol:↴")
+-- vim.g.indent_blankline_use_treesitter = true
+-- vim.g.indent_blankline_show_first_ident_level = false
 
 require("indent_blankline").setup {
     char = "|",
-    show_trailing_blankline_indent = false,
-    -- show_end_of_line = false,
+    use_treesitter = true,
+    show_trailing_blankline_indent = true,
+    show_first_indent_level = false,
     space_char_blankline = " ",
     show_current_context = true,
     show_current_context_start = true,
